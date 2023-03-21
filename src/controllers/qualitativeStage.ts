b311a4d555df6af24a08f57d7f31c787b931d97f
Miguel Carvalho Soares <doxito007@gmail.com>
2023-03-21 10:26:51 -0300

    Começo da Etapa qualitativa, criando o controler e Routes, ainda não terminado

---
+++ src/controllers/qualitativeStage.ts
@@ -0,0 +1,27 @@
+import { Request, Response } from "express";
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+class QualitativeStageController {
+    async get(req: Request, res: Response) {
+        const {idDelivery} = req.body
+        const deliveryProduct = await prisma.deliveriesProducts.findMany({
+            where: {
+                idDelivery: idDelivery
+            },
+            select: {
+                idProduct: true
+            }
+        })
+        const qualityProducts = await prisma.qualitiesProducts.findMany({
+            where: {
+                idProduct: { in: [deliveryProduct] },
+            },
+        })
+
+
+    }
+}
+
+export default new QualitativeStageController();
---
+++ src/routes/qualitativeStage.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+
+import { QualitativeStageController } from "../controllers";
+
+const routes = Router();
+
+
+
+export default routes;
