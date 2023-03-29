514c07abf07d2da7f9265295c4a336b6111fb364
Daniel <dmssjk@gmail.com>
2023-03-29 12:28:55 -0300

    criando a brach e iniciando a pagina quantitativa

--- src/app.ts
+++ src/app.ts
@@ -6,11 +6,7 @@ import router from "./routes";
 
 const app = express();
 
-app.use(
-    BodyParser.urlencoded(
-        {extended: true}
-    )
-);
+app.use(BodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
 app.use(router);
@@ -18,5 +14,10 @@ app.use(router);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  res.locals.title = "Abel's Home Page"; // THIS LINE IS KEY
+  res.render("home.ejs");
+});
 
 app.listen(3000);
