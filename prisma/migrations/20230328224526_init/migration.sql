2359812104dc19b9ed60868ca20caa11e17f5710
Otávio Abreu dos Santos Silva <otavio.abreu96@gmail.com>
2023-03-28 22:22:51 -0300

    criação do getProducts

---
+++ prisma/migrations/20230328224526_init/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `deliveriesProducts` MODIFY `actualWeight` DOUBLE NULL,
+    MODIFY `quantity` INTEGER NULL,
+    MODIFY `unit` VARCHAR(191) NULL,
+    MODIFY `specification` VARCHAR(191) NULL;
