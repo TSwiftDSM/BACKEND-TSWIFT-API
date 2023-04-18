871a17bd4d141c870cfdfea653f574f0d79e7ea6
Gabriel Santos <gabriel.santos@blitzpay.com.br>
2023-04-18 11:00:03 -0300

    formatar código

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,5 +1,3 @@
 export { default as Menu } from "./menu";
 export { default as QualitativeStageController } from "./qualitativeStage";
 export { default as EntregaController } from "./entregas";
-
-
