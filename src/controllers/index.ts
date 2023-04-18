aa9d8d6d30aeeebd7ed810e8579944a7bca0fb62
Gabriel Santos <gabriel.santos@blitzpay.com.br>
2023-04-18 19:36:40 -0300

    mudanças no get de entregas

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,5 @@
 export { default as QuantitativeController } from "./quantitativeStage";
 export { default as QualitativeStageController } from "./qualitativeStage";
+export { default as EntregaController } from "./entregas";
 export { default as EntradaMaterial } from "./entradaMaterial";
 export { default as Menu } from "./menu";
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,12 @@ import { default as Menu } from "./menu";
 import { default as qualitativeStageRoutes } from "./qualitativeStage";
 import { default as EntradaMaterial } from "./entradaMaterial";
 import { default as quantitativeRoutes } from "./quantitative";
+import { default as entregas } from "./entregas";
 
 const router = Router();
 
 router.use("/menu", Menu);
+router.use("/entregas", entregas);
 router.use("/conferencia/quantitativa", quantitativeRoutes);
 router.use("/qualitativeStage", qualitativeStageRoutes);
 router.use("/entradaMaterial", EntradaMaterial);
