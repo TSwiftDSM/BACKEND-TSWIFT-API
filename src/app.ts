235f1b749ad4224a9997de90d1e052f955a1d77c
Gabriel Santos <gabriel.santos@blitzpay.com.br>
2023-04-18 17:42:45 -0300

    resolver conflitos

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,18 @@
 import express from "express";
-<<<<<<< Updated upstream
 import BodyParser from "body-parser";
-=======
 import cors from "cors";
->>>>>>> Stashed changes
-//import BodyParser from "body-parser";
 
 import router from "./routes";
 
 const app = express();
 
-app.use(
-<<<<<<< Updated upstream
-    BodyParser.urlencoded(
-        {extended: true}
-    )
-);
-
+app.use(BodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(cors({ origin: "http://localhost:3000" }));
 
 app.use(router);
 
-app.use(express.static("public"));
-
-app.listen(3000, ()=>{
-    console.log('Ouvindo a porta 3000')
+app.listen(3000, () => {
+  console.log("Ouvindo a porta 3000");
 });
- 
-=======
-  cors({
-    origin: "http://localhost:5173",
-  })
-);
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(router);
-
-app.listen(3000);
->>>>>>> Stashed changes
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import  {default as Menu }  from "./menu";
+import { default as Menu } from "./menu";
 import { default as qualitativeStageRoutes } from "./qualitativeStage";
 import { default as quantitativeRoutes } from "./quantitative";
 
 const router = Router();
 
-router.use("/menu", Menu)
+router.use("/menu", Menu);
 router.use("/conferencia/quantitativa", quantitativeRoutes);
 router.use("/qualitativeStage", qualitativeStageRoutes);
 
