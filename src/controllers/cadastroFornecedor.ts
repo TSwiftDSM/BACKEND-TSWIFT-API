486873d9611d29b5e4582b2d83b4ade77ff3d916
Vitor Garcez <vitaoog@gmail.com>
2023-04-18 00:26:27 -0300

    Adição validação CNPJ já cadastrado

--- src/controllers/cadastroFornecedor.ts
+++ src/controllers/cadastroFornecedor.ts
@@ -3,23 +3,23 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient();
 
-async function cadastrarFornecedor(nomeFantasia:string,fornecedorCNPJ:string,razaoSocial:string,endereco:string){
-    try{
-      const cadastrarFornecedor =await prisma.fornecedor.create({
-        data: {
-          nomeFantasia: nomeFantasia ,
-          fornecedorCNPJ: fornecedorCNPJ,
-          razaoSocial: razaoSocial,
-          endereco: endereco,
-          transportadora: true,
-          fornecedor: true,
-        },
-      })
-    }catch(exception){
-        console.log(`Uma exceção ocorreu: ${exception}`)
-        return {}
-    }
-  };
+// async function cadastrarFornecedor(nomeFantasia:string,fornecedorCNPJ:string,razaoSocial:string,endereco:string){
+//     try{
+//       const cadastrarFornecedor =await prisma.fornecedor.create({
+//         data: {
+//           nomeFantasia: nomeFantasia ,
+//           fornecedorCNPJ: fornecedorCNPJ,
+//           razaoSocial: razaoSocial,
+//           endereco: endereco,
+//           transportadora: false,
+//           fornecedor: true,
+//         },
+//       })
+//     }catch(exception){
+//         console.log(`Uma exceção ocorreu: ${exception} CNPJ já cadastrado`)
+//         return res.json("CNPJ já cadastrado")
+//     }
+//   };
 
 class CadastroFornecedor{
     async post (req: Request, res: Response) {
@@ -27,7 +27,21 @@ class CadastroFornecedor{
         const fornecedorCNPJ = req.body.fornecedorCNPJ;
         const razaoSocial = req.body.razaoSocial;
         const endereco = req.body.endereco;
-        cadastrarFornecedor(nomeFantasia,fornecedorCNPJ,razaoSocial,endereco);
+        try{
+          const cadastrarFornecedor = await prisma.fornecedor.create({  //Função para cadastrar Fornecedor
+            data: {
+              nomeFantasia: nomeFantasia ,
+              fornecedorCNPJ: fornecedorCNPJ,
+              razaoSocial: razaoSocial,
+              endereco: endereco,
+              transportadora: false,
+              fornecedor: true,
+            },
+          })
+        }catch(exception){
+            console.log(`Uma exceção ocorreu: ${exception} CNPJ já cadastrado`)
+            return res.json("CNPJ já cadastrado").status(400) //Se ocorrer uma exeção retorna 'CNPJ já cadastrado'
+        }
         res.json("Fornecedor Cadastrado").status(200);
     };
 }
