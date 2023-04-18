81d8669f463047819689b9fb86b67b816d564c34
Gabriel Santos <gabriel.santos@blitzpay.com.br>
2023-04-18 11:53:00 -0300

    adicionar cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 import express from "express";
+<<<<<<< Updated upstream
 import BodyParser from "body-parser";
+=======
+import cors from "cors";
+>>>>>>> Stashed changes
 //import BodyParser from "body-parser";
 
 import router from "./routes";
@@ -7,6 +11,7 @@ import router from "./routes";
 const app = express();
 
 app.use(
+<<<<<<< Updated upstream
     BodyParser.urlencoded(
         {extended: true}
     )
@@ -22,3 +27,14 @@ app.listen(3000, ()=>{
     console.log('Ouvindo a porta 3000')
 });
  
+=======
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(router);
+
+app.listen(3000);
+>>>>>>> Stashed changes
