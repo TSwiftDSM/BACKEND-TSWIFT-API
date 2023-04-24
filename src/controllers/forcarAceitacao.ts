4512aa80faa795bf1a71c4a1c91637d36ae48421
Gabriel Santos <gabriel.santos@blitzpay.com.br>
2023-04-24 20:19:50 -0300

    arrumar forçar recebimento

--- src/controllers/forcarAceitacao.ts
+++ src/controllers/forcarAceitacao.ts
@@ -3,10 +3,6 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-type StatusEntrega = {
-  id: number;
-};
-
 async function buscarIdStatusEntrega(entregaId: number) {
   try {
     const statusEntrega = await prisma.statusEntrega.findMany({
@@ -21,26 +17,26 @@ async function buscarIdStatusEntrega(entregaId: number) {
       throw new Error(
         `Nenhum resultado encontrado para entregaId: ${entregaId}`
       );
-
     }
-    const idStatusEntrega = statusEntrega[0]?.id;
-    // return JSON.stringify({ id: idStatusEntrega });
-    return { id: idStatusEntrega };
+    const idStatusEntrega = statusEntrega.map((s) => s.id);
+    return idStatusEntrega;
   } catch (exception) {
     console.log(`Uma exceção ocorreu: ${exception}`);
-    return {};
+    return [];
   }
 }
 
 async function alterarStatusEntrega(
-  idStatusEntrega: number,
+  idStatusEntrega: Array<number>,
   usuarioId: number
 ) {
   try {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    const alterarStatusEntrega = await prisma.statusEntrega.update({
+    await prisma.statusEntrega.updateMany({
       where: {
-        id: idStatusEntrega,
+        id: {
+          in: idStatusEntrega,
+        },
       },
       data: {
         aprovado: true,
@@ -57,10 +53,10 @@ class ForcarAceitacaoController {
   async post(req: Request, res: Response) {
     const entregaId = parseInt(req.params.entregaId); // pega a entregaId pelo parametro da url
     const usuarioId = parseInt(req.params.usuarioId); // pega a usuarioId pelo parametro da url
-    const idStatusEntrega = await buscarIdStatusEntrega(entregaId); //busca o id do statusEntrega
-    const idObj = JSON.parse(JSON.stringify(idStatusEntrega)) as StatusEntrega; // pegar o id da função buscarIdStatusEntrega pelo atributo id
-    const idInt = idObj.id; // transforma o id em inteiro
-    await alterarStatusEntrega(idInt, usuarioId); // ele altera o status de reprovado para aprovado de acordo com o id do status
+    const idStatusEntrega: Array<number> = await buscarIdStatusEntrega(
+      entregaId
+    ); //busca o id do statusEntrega
+    await alterarStatusEntrega(idStatusEntrega, usuarioId); // ele altera o status de reprovado para aprovado de acordo com o id do status
     res.send("Forçar Aceitação concluida").status(200);
   }
 }
