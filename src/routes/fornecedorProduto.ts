b04e0f95e5040e6634e88a7d2c0e7a5e4a09708f
Miguel Carvalho <doxito007@gmail.com>
2023-04-28 18:26:43 -0300

    Adicionando Rotas

--- src/routes/fornecedorProduto.ts
+++ src/routes/fornecedorProduto.ts
@@ -4,6 +4,10 @@ import { FornecedorProduto } from "../controllers";
 const router = Router();
 
 router.get("/", FornecedorProduto.get)
+router.get("/:id", FornecedorProduto.getPorIdFornecedor)
+router.get("/:idProduto/:idFornecedor", FornecedorProduto.getPorIdProdutoFornecedor)
+router.post("/", FornecedorProduto.post)
+router.delete("/:idProduto/:idFornecedor", FornecedorProduto.delete)
 
 
 export default router;
