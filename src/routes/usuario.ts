648ae7938216cc42d95ce4b4d15fc92487daa116
Vitor Garcez <vitaoog@gmail.com>
2023-04-28 18:08:45 -0300

    Adição getPorNome CRUD Usuarios

--- src/routes/usuario.ts
+++ src/routes/usuario.ts
@@ -5,6 +5,7 @@ const routes = Router();
 
 routes.get("/", UsuarioController.get);
 routes.get("/tipoUsuario/:tipoUsuarioId", UsuarioController.getPorTipoUsuario);
+routes.get("/pesquisa", UsuarioController.getPorNome);
 routes.get("/id/:idUsuario", UsuarioController.getPorId);
 routes.delete("/:idUsuario", UsuarioController.delete);
 routes.put("/:idUsuario", UsuarioController.update);
