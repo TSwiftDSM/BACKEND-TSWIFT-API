3424856d16d9b09646cb7bbc6edbfb3aa7f118f9
Otávio Abreu dos Santos Silva <otavio.abreu96@gmail.com>
2023-04-28 13:59:12 -0300

    rodando lint e format para o QualidadeProduto

--- src/services/serviceQualidadeProduto.ts
+++ src/services/serviceQualidadeProduto.ts
@@ -4,112 +4,112 @@ const prisma = new PrismaClient();
 
 // Formatos do Objeto PUT
 interface putBodyObj {
-    obrigatorio: boolean
+  obrigatorio: boolean;
 }
 
 // Formato de Objeto Padrão
 interface objQualidadeProduto {
-    obrigatorio: boolean,
-    produtoId: number,
-    testeQualidadeId: number
+  obrigatorio: boolean;
+  produtoId: number;
+  testeQualidadeId: number;
 }
 
 class ServiceQualidadeProduto {
-    public async getQualidadeProduto(mod: string, id: number){
-        if (mod == "p"){
-            try {
-                const resp = await this.getProdutoQualidadeProduto(id);
-                return resp;
-            } catch(exception) {
-                console.log(exception)
-                return 400;
-            }
-        } else {
-            try {
-                const resp = await this.getQualidadeQualidadeProduto(id);
-                return resp;
-            } catch(exception) {
-                console.log(exception)
-                return 400
-            }
-        }
-    }
-
-    private async getProdutoQualidadeProduto(id: number) {
-            const resp = await prisma.qualidadeProduto.findMany({
-                where:{
-                    produtoId: id
-                }
-            })
-
-            return resp;
-    }
-
-    private async getQualidadeQualidadeProduto(id: number) {
-        const resp = await prisma.qualidadeProduto.findMany({
-            where:{
-                testeQualidadeId: id
-            }
-        })
-
+  public async getQualidadeProduto(mod: string, id: number) {
+    if (mod == "p") {
+      try {
+        const resp = await this.getProdutoQualidadeProduto(id);
         return resp;
+      } catch (exception) {
+        console.log(exception);
+        return 400;
+      }
+    } else {
+      try {
+        const resp = await this.getQualidadeQualidadeProduto(id);
+        return resp;
+      } catch (exception) {
+        console.log(exception);
+        return 400;
+      }
     }
+  }
 
-    public async postQualidadeProduto(obj: objQualidadeProduto){
-        try{
-            await prisma.qualidadeProduto.create({
-                data: {
-                    obrigatorio: obj.obrigatorio,
-                    testeQualidadeId: obj.testeQualidadeId,
-                    produtoId: obj.produtoId
-                }
-            });
+  private async getProdutoQualidadeProduto(id: number) {
+    const resp = await prisma.qualidadeProduto.findMany({
+      where: {
+        produtoId: id,
+      },
+    });
 
-            return 201;
-        } catch(exception) {
-            console.log(exception);
-            return 400;
-        }
+    return resp;
+  }
+
+  private async getQualidadeQualidadeProduto(id: number) {
+    const resp = await prisma.qualidadeProduto.findMany({
+      where: {
+        testeQualidadeId: id,
+      },
+    });
+
+    return resp;
+  }
+
+  public async postQualidadeProduto(obj: objQualidadeProduto) {
+    try {
+      await prisma.qualidadeProduto.create({
+        data: {
+          obrigatorio: obj.obrigatorio,
+          testeQualidadeId: obj.testeQualidadeId,
+          produtoId: obj.produtoId,
+        },
+      });
+
+      return 201;
+    } catch (exception) {
+      console.log(exception);
+      return 400;
     }
+  }
 
-    public async putQualidadeProduto(query: any, body: putBodyObj){
-        try{
-            await prisma.qualidadeProduto.update({
-                where: {
-                    testeQualidadeId_produtoId:{
-                        testeQualidadeId: Number(query.q),
-                        produtoId: Number(query.p)
-                    }
-                },
-                data: {
-                  obrigatorio: body.obrigatorio,
-                },
-            });
+  public async putQualidadeProduto(query: any, body: putBodyObj) {
+    try {
+      await prisma.qualidadeProduto.update({
+        where: {
+          testeQualidadeId_produtoId: {
+            testeQualidadeId: Number(query.q),
+            produtoId: Number(query.p),
+          },
+        },
+        data: {
+          obrigatorio: body.obrigatorio,
+        },
+      });
 
-            return 200;
-        } catch(exception) {
-            console.log(exception);
-            return 400;
-        };
+      return 200;
+    } catch (exception) {
+      console.log(exception);
+      return 400;
     }
+  }
 
-    public async deleteQualidadeProduto(query: any){
-        try{
-            await prisma.qualidadeProduto.delete({
-                where: {
-                    testeQualidadeId_produtoId:{
-                        testeQualidadeId: Number(query.q),
-                        produtoId: Number(query.p)
-                    }
-                }
-            });
-            
-            return 200;
-        } catch(exception) {
-            console.log(exception);
-            return 400;
-        }
+  public async deleteQualidadeProduto(query: any) {
+    try {
+      await prisma.qualidadeProduto.delete({
+        where: {
+          testeQualidadeId_produtoId: {
+            testeQualidadeId: Number(query.q),
+            produtoId: Number(query.p),
+          },
+        },
+      });
+
+      return 200;
+    } catch (exception) {
+      console.log(exception);
+      return 400;
     }
+  }
 }
 
 export default new ServiceQualidadeProduto();
