284f5c54a1fb27516bc588b95beb83622122eccc
Gabriel Santos <gabriel.santos@blitzpay.com.br>
2023-05-03 10:42:15 -0300

    permitir temporariamente cadastro de pedido

--- src/schemas/entregas.ts
+++ src/schemas/entregas.ts
@@ -2,8 +2,6 @@ import Joi from "joi";
 
 class EntregaSchema {
   readonly joi = Joi.object({
-    nfe: Joi.string().required(),
-    numeroPedido: Joi.string().required(),
     tipoFrete: Joi.string().required(),
     formaPagamento: Joi.string().required(),
     etapaEntrega: Joi.string(),
