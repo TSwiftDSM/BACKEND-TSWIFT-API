b7211c8a18eb1f3c091105b6572f93cb03d58c8d
Miguel <doxito007@gmail.com>
2023-05-22 10:31:38 -0300

    Adicionando permissao e PermissaoUsuario no teste de massa

--- prisma/test_mass.sql
+++ prisma/test_mass.sql
@@ -456,3 +456,40 @@ VALUES
         curdate(),
         3
     );
+    
+INSERT INTO permissoes(descricao) 
+VALUES ('PRODUTOS');
+INSERT INTO permissoes(descricao) 
+VALUES ('FORNECEDORES');
+INSERT INTO permissoes(descricao) 
+VALUES ('PEDIDOS');
+INSERT INTO permissoes(descricao) 
+VALUES ('COLABORADORES');
+INSERT INTO permissoes(descricao) 
+VALUES ('RECEBIMENTO');
+INSERT INTO permissoes(descricao) 
+VALUES ('REGRAS');
+INSERT INTO permissoes(descricao) 
+VALUES ('TRANSPORTADORA');	
+
+INSERT INTO permissoesusuario(permissaoId, usuarioId)
+VALUES(1,1);
+INSERT INTO permissoesusuario(permissaoId, usuarioId)
+VALUES(2,1);
+INSERT INTO permissoesusuario(permissaoId, usuarioId)
+VALUES(3,1);
+INSERT INTO permissoesusuario(permissaoId, usuarioId)
+VALUES(4,1);
+INSERT INTO permissoesusuario(permissaoId, usuarioId)
+VALUES(5,1);
+INSERT INTO permissoesusuario(permissaoId, usuarioId)
+VALUES(6,1);
+INSERT INTO permissoesusuario(permissaoId, usuarioId)
+VALUES(7,1);
+
+SELECT * FROM permissoes; 
+
+SELECT * FROM permissoesusuario;
+
+SELECT * FROM usuarios;
+
